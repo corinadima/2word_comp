c9154a9c641711c4567dce9e72c7a3a40d1fdbb5
Corina Dima <corina.dima@uni-tuebingen.de>
2018-03-29 12:44:43 +0200

    Lex models

---
+++ composition/composition_models/LexicalFunction.lua
@@ -0,0 +1,65 @@
+local compose_utils = require 'utils.compose_utils'
+local nninit = require 'nninit'
+
+--
+-- LexicalFunction composition model
+--
+-- 
+
+local LexicalFunction, parent = torch.class('torch.LexicalFunction', 'torch.CompositionModel')
+
+function LexicalFunction:__init(inputs, outputs, vocab_size)
+	parent.__init(self)
+	self.inputs = inputs
+	self.outputs = outputs
+	self.vocab_size = vocab_size
+	self.name = "lexfunc"
+	self.isTrainable = true
+	self.iteratorType = 'table'
+end
+
+function LexicalFunction:architecture(config)
+	print("# LexicalFunction; vector v as input; modification matrix U is learned for each word individually; p = U * v ")
+	print("# inputs " .. self.inputs .. ", outputs " .. self.outputs)
+
+	self.config = config
+
+	local ltInit = torch.Tensor(self.vocab_size, self.inputs/2 * self.inputs/2)
+	for i = 1, self.vocab_size do
+		local lt = nn.Linear(self.inputs/2, self.inputs/2):init('weight', nninit.eye):init('weight', nninit.addNormal, 0, 1e-4)
+		ltInit[i] = lt.weight
+	end
+
+	local u = nn.Identity()(); -- index
+	local v = nn.Identity()(); -- vector
+
+	local sel = nn.SelectTable(2)({u,v});
+
+	local U = nn.LookupTable(self.vocab_size,self.inputs/2*self.inputs/2):init('weight', 
+					nninit.copy, ltInit)(u):annotate{name='U'}
+	self.U = U;
+
+	local reshape = nn.Reshape(self.inputs/2,self.inputs/2,true)({U})
+
+	local mul = nn.MM()({reshape, sel})
+	local reshape_mul = nn.Reshape(self.outputs,true)({mul})
+
+	self.mlp = nn.gModule({u,v}, {reshape_mul})
+
+
+	print("==> Network configuration")
+	print(self.mlp)
+	print("==> Parameters size")
+	print(self.mlp:getParameters():size())
+
+	collectgarbage()
+
+	return self.mlp
+end
+
+function LexicalFunction:data(trainSet, devSet, testSet, fullSet, cmhEmbeddings)
+	self.trainDataset = compose_utils:createCMHM1V2Dataset(trainSet, cmhEmbeddings)
+	self.testDataset = compose_utils:createCMHM1V2Dataset(testSet, cmhEmbeddings)
+	self.devDataset = compose_utils:createCMHM1V2Dataset(devSet, cmhEmbeddings)
+	self.fullDataset = compose_utils:createCMHM1V2Dataset(fullSet, cmhEmbeddings)
+end
---
+++ composition/composition_models/TestLex.lua
@@ -0,0 +1,84 @@
+require 'cutorch'
+require 'cunn'
+require 'cudnn'
+require 'nngraph'
+
+require 'composition/composition_models.CompositionModel'
+require 'composition/composition_models.LexicalFunction'
+require 'composition/composition_models.FullLex'
+
+require 'iterators.BatchIterator'
+
+local composition_tests = torch.TestSuite()
+local tester = torch.Tester()
+
+local gpuid = 1
+print('using CUDA on GPU ' .. gpuid .. '...')
+cutorch.setDevice(gpuid)
+print('Running on device: ' .. cutorch.getDeviceProperties(cutorch.getDevice()).name)
+
+
+function composition_tests.LexicalFunction()
+	local sz = 5
+	local config = {batchSize = 2}
+	local vocabSize = 2
+	local composition_model = torch.LexicalFunction(sz * 2, sz, vocabSize)
+	local mlp = composition_model:architecture(config)
+	mlp:cuda()
+
+	local u = torch.Tensor(2):cuda()
+	u[1] = torch.Tensor({1})
+	u[2] = torch.Tensor({2})
+	local v = torch.Tensor(2,5,1):cuda()
+	v[1] = torch.Tensor({6,7,8,9,10})
+	v[2] = torch.Tensor({7,8,9,10,11})
+	local input = {u, v}
+	local output = mlp:forward(input)
+
+	local expected_output = torch.Tensor(2,5):cuda()
+	expected_output[1] = torch.Tensor{6,7,8,9,10}
+	expected_output[2] = torch.Tensor{7,8,9,10,11}
+	tester:assertTensorEq(output, expected_output, 1e-2, "output and expected_output should be equal")
+end
+
+function composition_tests.FullLex()
+	local sz = 5
+	local config = {batchSize = 2}
+	local vocabSize = 2
+	local composition_model = torch.FullLex(sz * 2, sz, vocabSize)
+	local mlp = composition_model:architecture(config)
+	mlp:cuda()
+
+    for _, node in ipairs(mlp.forwardnodes) do
+      	if node.data.annotations.name == 'W' then
+        	node.data.module.weight:fill(1.0)
+    	end
+    end
+
+	local u_idx = torch.Tensor(2):cuda()
+	u_idx[1] = torch.Tensor({1})
+	u_idx[2] = torch.Tensor({2})
+	local u = torch.Tensor(2,5,1):cuda()
+	u[1] = torch.Tensor({1,2,3,4,5})
+	u[2] = torch.Tensor({3,4,5,6,7})
+
+	local v_idx = torch.Tensor(2):cuda()
+	v_idx[1] = torch.Tensor({2})
+	v_idx[2] = torch.Tensor({1})
+	local v = torch.Tensor(2,5,1):cuda()
+	v[1] = torch.Tensor({6,7,8,9,10})
+	v[2] = torch.Tensor({7,8,9,10,11})
+	local input = {u_idx, u, v_idx, v}
+	local output = mlp:forward(input)
+
+	-- the tanh makes everything one - because the vector elements are large
+	local expected_output = torch.Tensor(2,5):cuda()
+	expected_output[1] = torch.Tensor{1,1,1,1,1}
+	expected_output[2] = torch.Tensor{1,1,1,1,1}
+	tester:assertTensorEq(output, expected_output, 1e-2, "output and expected_output should be equal")
+end
+
+
+
+tester:add(composition_tests)
+tester:run()
